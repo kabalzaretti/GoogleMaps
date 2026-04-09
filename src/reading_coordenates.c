6dc5f4311f4e58e2be522450ee4d8fd48b131669
oriol_blanque <oriol.blanque01@estudiant.upf.edu>
2026-04-09 09:47:48 +0200

    Quatre primers punts del lab 2 sense acabar

--- src/reading_coordenates.c
+++ src/reading_coordenates.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include
 
 char* read_map(){ 
-    char* map;      
+    char* map;
     printf("Enter map name (e.g. 'xs_2' or 'xl_1'): ");
     scanf("%s",map);
+    while(map != "xs_1" || map != "xs_2" || map != "md_1" || map != "lg_1" || map != "xl_1" || map != "2xl_1"){   
+        printf("Arxive not correct, please repeat");
+        scanf("%s",map);
+    }
+    return map;
+}
 
+void posicio_origen(){
+    char* posicio;
+    printf("Where are you? Address (1), Place (2) or Coordinate (3)?");
+    scanf("%s",posicio);
+    while(posicio != 1 || posicio != 2 || posicio != 3){
+        printf("Answer not valid, please repeat");
+        scanf("%s",posicio);
+    }
 
+    if (posicio == 2 || posicio == 3){
+        printf("Not implemented yet")
+    }
+    else if(posicio == 1){
+        char coordenades[2][150];
+        printf("Enter your adress and house number");
+        scanf("%s %s",coordenades);
+        printf("")
+    }
+    
     
 }
